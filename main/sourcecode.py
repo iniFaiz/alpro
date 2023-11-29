5b79c0a2a6d7b6ac92a5eb4717b345eb8c83fe1e
faiz <64961002+iniFaiz@users.noreply.github.com>
2023-11-29 11:01:32 +0800

    Update sourcecode.py

--- main/sourcecode.py
+++ main/sourcecode.py
@@ -2,9 +2,13 @@ import tkinter as tk
 import customtkinter as ctk
 import time
 import csv
+import datetime
 from win10toast import ToastNotifier
 from tkinter import *
- 
+from threading import Thread
+
+
+
 app = ctk.CTk()
 app.title("organize")
 app.geometry("900x600")
@@ -27,19 +31,11 @@ main_content = ctk.CTkFrame(master=app,
                             fg_color="#A9CCE3", 
                             corner_radius=0)
 main_content.pack(side='right', fill='both', expand=True)
+main_label = ctk.CTkLabel(master=main_content, text="Organize\nManajemen waktu bersama kami",
+                         font=('Arial Bold', 34), text_color='#0d1117')
+main_label.place(x=100,y=250)
 
 # function
-def todolist():
-    todolist = ctk.CTkFrame(master = main_content)
-    label = ctk.CTkLabel(master = todolist,
-                        fg_color = '#c9d1d9',
-                        font = ('Arial Bold', 40),
-                        width = 640,
-                        height = 400
-                        )
-    label.pack(fill = 'both')
-    todolist.pack()
-    
 def delete_pages():
     for frame in main_content.winfo_children():
         frame.destroy()
@@ -92,83 +88,125 @@ analisis_btn = ctk.CTkButton(sidebar,
                              corner_radius=8, 
                              fg_color="#75A5D0", 
                              hover_color="#5084B9", 
-                             height=50)
+                             height=50,
+                             command= lambda: indicate(analisis_btn, analisis_page))
 analisis_btn.pack(pady=20, padx=20, fill='x')
 
-#page pomodoro
 def pomodoro_page():
-    
-    pomodoro_timer_label = ctk.CTkLabel(main_content, 
-                                    text="25:00", 
-                                    font=("Helvetica", 180))
-    pomodoro_timer_label.pack(pady=100)
- 
+    global notifier
+    global timer_seconds
+    global rest_time
+    global timer_running
+    global timer_label
+    global start_button
+    global add_button
+    global subtract_button
+    global stop_button
+    global resume_button
+    global reset_button
+    #notifier
+    notifier = ToastNotifier()
 
-    pomodoro_running = False
-    pomodoro_time = 1500 #dalam detik = 25 menit
-    # Tombol up untuk memulai pomodoro
-    up_button = ctk.CTkButton(main_content, 
-                          text="▶", 
-                          width=80, 
-                          height=80, 
-                          corner_radius=20, 
-                          command=start_pomodoro)
-    up_button.pack(side='left', padx=110)
- 
-    # Tombol down untuk menghentikan pomodoro
-    reset_button = ctk.CTkButton(main_content, 
-                            text="■", 
-                            width=80, 
-                            height=80, 
-                            corner_radius=20, 
-                            command=stop_pomodoro)
-    reset_button.pack(side='right', padx=110)
- 
-    # Tombol pause (||)
-    pause_button = ctk.CTkButton(main_content, 
-                             text="||", 
-                             width=80, 
-                             height=80, 
-                             corner_radius=20, 
-                             command=pause_pomodoro)
-    pause_button.pack(side='right', padx=20)
+    #values
+    timer_seconds = 30 * 60  # 30 minutes
+    rest_time = False
+    timer_running = False
 
-# Fungsi untuk memulai pomodoro
-def start_pomodoro():
-    global pomodoro_running, pomodoro_time
-    if not pomodoro_running:
-        pomodoro_running = True
-        update_pomodoro_timer()
- 
-# Fungsi untuk melakukan pause pada pomodoro
-def pause_pomodoro():
-    global pomodoro_running
-    pomodoro_running = False
- 
-# Fungsi untuk menghentikan pomodoro
-def stop_pomodoro():
-    global pomodoro_running, pomodoro_time
-    pomodoro_running = False
-    pomodoro_time = 1500
-    pomodoro_timer_label.configure(text="25:00")
- 
- 
-# Fungsi untuk mengupdate timer pomodoro
-def update_pomodoro_timer():
-    global pomodoro_running, pomodoro_time
-    if pomodoro_running and pomodoro_time > 0:
-        minutes, seconds = divmod(pomodoro_time, 60)
-        timer_text = "{:02d}:{:02d}".format(minutes, seconds)
-        pomodoro_timer_label.configure(text=timer_text)
-        pomodoro_time -= 1
-        app.after(1000, update_pomodoro_timer)
-    elif pomodoro_running and pomodoro_time == 0:
-        pomodoro_running = False
-        pomodoro_timer_label.configure(text="25:00")
-        toaster.show_toast("Pomodoro", "Waktu pomodoro sudah habis. Istirahat sejenak!", duration=300)
- 
-# Fungsi untuk menampilkan notifikasi
-toaster = ToastNotifier()
+    # Timer label
+    timer_label = ctk.CTkLabel(main_content, pady=150, text="30:00", font=("Arial", 100))
+    timer_label.pack(pady=20)
+
+    # Buttons
+    start_button = ctk.CTkButton(main_content, width=80, height=80, corner_radius=20,text="▶", command=start_timer)
+    add_button = ctk.CTkButton(main_content, width=80, height=80, corner_radius=20,text="+", command=add_time)
+    subtract_button = ctk.CTkButton(main_content, width=80, height=80, corner_radius=20,text="-", command=subtract_time)
+    stop_button = ctk.CTkButton(main_content, text="Stop", command=stop_timer)
+    resume_button = ctk.CTkButton(main_content, text="Resume", command=resume_timer)
+    reset_button = ctk.CTkButton(main_content, text="Reset", command=reset_timer)
+
+    start_button.pack(side='left', padx=10, pady=10)
+    add_button.pack(side='left', padx=210, pady=10)
+    subtract_button.pack(side='right', padx=10, pady=10)
+
+def update_timer():
+    global timer_running, timer_seconds, rest_time
+    while timer_running:
+        time.sleep(1)
+        timer_seconds -= 1
+
+        if timer_seconds <= 0:
+            if rest_time:
+                timer_running = False
+                notifier.show_toast("Pomodoro Timer", "Waktu istirahat selesai!", duration=5)
+            else:
+                notifier.show_toast("Pomodoro Timer", "Waktu bekerja selesai, mulai istirahat!", duration=5)
+                timer_seconds = 5 * 60  # Istirahat 5 menit
+                rest_time = True
+        else:
+            if not rest_time and timer_seconds == (30 * 60):
+                notifier.show_toast("Pomodoro Timer", "30 menit telah lewat, mulai istirahat!", duration=5)
+                timer_seconds = 5 * 60  # Istirahat 5 menit
+                rest_time = True
+        if not timer_running:
+            break
+        minutes, seconds = divmod(timer_seconds, 60)
+        timer_label.configure(text=f"{minutes:02d}:{seconds:02d}")
+
+def start_timer():
+    global timer_running
+    timer_running = True
+    thread = Thread(target=update_timer)
+    thread.start()
+    start_button.pack_forget()
+    add_button.pack_forget()
+    subtract_button.pack_forget()
+    stop_button.pack(pady=10)
+    reset_button.pack(pady=10)
+    resume_button.pack_forget()
+
+def stop_timer():
+    global timer_running
+    timer_running = False
+    stop_button.pack_forget()
+    resume_button.pack(pady=10)
+
+def resume_timer():
+    global timer_running
+    timer_running = True
+    thread = Thread(target=update_timer)
+    thread.start()
+    resume_button.pack_forget()
+    stop_button.pack(pady=10)
+
+def reset_timer():
+    global timer_seconds, rest_time, timer_running
+    timer_running = False
+    timer_seconds = 30 * 60
+    rest_time = False
+    update_timer_label()
+    stop_button.pack_forget()
+    reset_button.pack_forget()
+    resume_button.pack_forget()
+    start_button.pack(side='left', padx=10, pady=10)
+    add_button.pack(side='left', padx=210, pady=10)
+    subtract_button.pack(side='right', padx=10, pady=10)
+
+def add_time():
+    global timer_seconds
+    if not rest_time:
+        timer_seconds += 30 * 60
+        update_timer_label()
+
+def subtract_time():
+    global timer_seconds
+    if not rest_time and timer_seconds > 30 * 60:
+        timer_seconds -= 30 * 60
+        update_timer_label()
+
+def update_timer_label():
+    minutes, seconds = divmod(timer_seconds, 60)
+    timer_label.configure(text=f"{minutes:02d}:{seconds:02d}")
+
  
 def todo_page():
     global task_listbox
@@ -187,6 +225,23 @@ def todo_page():
     # Tombol untuk menghapus task yang dipilih
     delete_task_btn = ctk.CTkButton(todo_frame, text="Delete Task", command=delete_task)
     delete_task_btn.pack(side="bottom", pady=10)
+    
+    load_datatodo()
+
+# database todolist
+def add_data():
+    item = task_entry.get().strip()
+    if item:
+        task_listbox.insert(tk.END, item)
+        data_todo()
+
+# save data
+def data_todo():
+    with open('todolist.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        for i in range(task_listbox.size()):
+            writer.writerow([task_listbox.get(i)])
+    
 
 # Fungsi untuk menambahkan task
 def add_task():
@@ -201,6 +256,16 @@ def delete_task():
     if selected_task_index:
         task_listbox.delete(selected_task_index)
 
+#load database todo
+def load_datatodo():
+    try:
+        with open('todolist.csv', 'r', newline= '') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                task_listbox.insert(tk.END, row[0])
+    except FileNotFoundError:
+        pass
+
 # Membuat pop-up untuk menambahkan task
 def open_add_task_popup():
     popup = ctk.CTkToplevel(app)
@@ -210,7 +275,7 @@ def open_add_task_popup():
     task_entry = ctk.CTkEntry(popup, width=200)
     task_entry.pack(pady=10)
     
-    add_task_button = ctk.CTkButton(popup, text="Add Task", command=add_task)
+    add_task_button = ctk.CTkButton(popup, text="Add Task", command=add_data)
     add_task_button.pack()
 
 def stopwatch_page():
@@ -266,60 +331,8 @@ def reset_stopwatch():
     stopwatch_counter = 0
     time_label.configure(text="00:00:00.00")
 
+def analisis_page():
+    global analisis_frame
+    analisis_frame = ctk.CTkFrame(master=main_content)
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Label untuk To-Do-List
-# todo_label = ctk.CTkLabel(main_content, 
-#                           text="To-Do List", 
-#                           font=("Helvetica", 24))
-# todo_label.pack(pady=20)
- 
-# # Entry untuk memasukkan tugas baru
-# todo_entry = ctk.CTkEntry(main_content, 
-#                           font=("Helvetica", 14))
-# todo_entry.pack(pady=10)
- 
-# # Listbox untuk menampilkan daftar tugas
-# todo_listbox = tk.Listbox(main_content, 
-#                               font=("Helvetica", 14), 
-#                               selectmode=tk.SINGLE, 
-#                               height=10, width=40)
-# todo_listbox.pack(pady=10)
- 
-# # Fungsi untuk menambahkan tugas baru
-# def add_task():
-#     task = todo_entry.get()
-#     if task:
-#         todo_listbox.insert(tk.END, task)
-#         todo_entry.delete(0, tk.END)
- 
-# # Fungsi untuk menghapus tugas yang dipilih
-# def delete_task():
-#     selected_task_index = todo_listbox.curselection()
-#     if selected_task_index:
-#         todo_listbox.delete(selected_task_index)
- 
-# # Tombol untuk menambahkan tugas baru
-# add_task_button = ctk.CTkButton(main_content, 
-#                                 text="Add Task", 
-#                                 command=add_task)
-# add_task_button.pack(pady=10)
- 
-# # Tombol untuk menghapus tugas yang dipilih
-# delete_task_button = ctk.CTkButton(main_content, 
-#                                    text="Delete Task", 
-#                                    command=delete_task)
-# delete_task_button.pack(pady=10)
- 
 app.mainloop()
